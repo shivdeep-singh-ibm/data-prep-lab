0bc1e964e418c8c1c91e524173b5f933dc26e874
Shivdeep Singh <Shivdeep.Singh@ibm.com>
2024-07-26 05:05:18 -0500

    some changes
    Signed-off-by: Shivdeep Singh <Shivdeep.Singh@ibm.com>

--- data-processing-lib/ray/src/data_processing_ray/runtime/ray/transform_orchestrator.py
+++ data-processing-lib/ray/src/data_processing_ray/runtime/ray/transform_orchestrator.py
@@ -131,6 +131,8 @@ def orchestrate(
         logger.error(f"Exception during execution {e}: {traceback.print_exc()}")
         status = "failure"
         return_code = 1
+    del processors
+    del processors_pool
     try:
         # Compute execution statistics
         logger.debug("Computing execution stats")
